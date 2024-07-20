188b606a766e9352336c467ceabeaaca1da3e82b
Roman Potapov <rummaan@me.com>
2024-07-20 02:16:30 -0400

    v0001

---
+++ Hike/Component/CardView.swift
@@ -0,0 +1,76 @@
+//
+//  CardView.swift
+//  Hike
+//
+//  Created by Roman Potapov on 7/18/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        // MARK: - CARD
+        
+        ZStack {
+            CustomBackgroundView()
+            VStack {
+                // MARK: - HEADER
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(colors: [
+                                    .customGrayLight,
+                                        .customGrayMedium
+                                ],
+                                               startPoint: .top, 
+                                               endPoint: .bottom
+                                )
+                        )
+                        Spacer()
+                        Button {
+                            // ACTION: Show a sheet
+                            print("The button was pressed")
+                        } label: {
+                            CustomButtonView()
+                        }
+                    }
+                    Text("Fun and enjoyable outdoor activity for family and friends")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundStyle(Color.customGrayMedium)
+                } //: HEADER
+                .padding(.horizontal, 30)
+                
+                // MARK: - MAIN CONTENT
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [
+                                    Color("ColorIndigoMedium"),
+                                    Color("ColorSalmonLight")
+                                ],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                    .scaledToFit()
+                }
+                // MARK: - FOOTER
+            }
+        }
+        //: CARD
+        .frame(width: 320, height: 570)
+    }
+}
+
+#Preview {
+    CardView()
+}
